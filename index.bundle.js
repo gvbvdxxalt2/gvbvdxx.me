148530dc46d22834c425c2e2ec8abde5f4a41e9c
gvbvdxxalt2 <gvbvdxxalt2@outlook.com>
2026-04-20 16:54:24 +0000

    Deploying to deploy from @ gvbvdxxalt2/gvbvdxxalt2.github.io@78e97dbd9c1b901f7d6cbfa5f3b1ca9c738473c5 🚀

--- index.bundle.js
+++ index.bundle.js
@@ -353,8 +353,14 @@ function refreshProfileCategoriesButtons() {
 	elements.setInnerJSON(profileCategories,buttonElements);
 }
 
+function refreshProfileCategoriesContent() {
+	var story = profile.stories.find((story) => story.id == selectedId);
+	profileStoryContent.innerHTML = story.content;
+}
+
 function refreshProfileCategories() {
 	refreshProfileCategoriesButtons();
+	refreshProfileCategoriesContent();
 }
 
 profile.stories.forEach((story) => {
