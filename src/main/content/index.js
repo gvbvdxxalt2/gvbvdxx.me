36618b40704a9cff4f0111c8a409a20dae9f34b0
gvbvdxxalt2 <jgevans@students.wcpss.net>
2026-04-20 14:35:59 +0000

    Commit

--- src/main/content/index.js
+++ src/main/content/index.js
@@ -1,11 +1,14 @@
 var xml = require("../get-xml.js");
 
+//Everything will use innerHTML or getAttribute from the xml, just easiest way to write profiles without bloating the site with markdown parsers.
+
 var profile = {
 	name: "Name",
 	picture: "favicon.png",
 	links: [
 		{
 			name: "Link",
+			logo: "favicon.png",
 			href: "https://google.com", //Placeholder until data is read.
 		}
 	]
@@ -14,5 +17,33 @@ var profile = {
 var profilePages = [];
 var projects = [];
 
-var profile = xml.querySelector("profile");
-window.alert(""+profile);
+function readProfileSection(p) {
+	if (!p) {
+		return;
+	}
+	var name = p.querySelector("name").innerHTML;
+	var pfp = p.querySelector("pfp").getAttribute("src");
+	var bg = p.querySelector("bg").getAttribute("css");
+	
+	profile.name = name;
+	profile.picture = pfp;
+	profile.cssBg = bg;
+
+	var socialLinks = p.querySelector("social");
+	var linksArray = [];
+	for (var link of socialLinks.children) {
+		linksArray.push({
+			name: link.getAttribute("label"),
+			href: link.getAttribute("goto"),
+			logo: link.getAttribute("logo"),
+		});
+	}
+
+	profile.links = linksArray;
+}
+
+readProfileSection(xml.querySelector("profile"));
+
+module.exports = {
+	profile	
+};
--- src/main/jsondoc/about-me-left.js
+++ src/main/jsondoc/about-me-left.js
@@ -1,7 +1,31 @@
+var {profile} = require("../content");
+
 module.exports = [
 	{
 		element: "div",
 		gid: "aboutMeLeft",
-		className: "aboutMeLeft"
+		className: "aboutMeLeft",
+		children: [
+			{
+				element: "div",
+				gid: "aboutMeProfileName",
+				className: "aboutMeProfileName",
+				style: {
+					background: profile.cssBg,
+				},
+				children: [
+					{
+						element: "img",
+						className: "profilePicture",
+						src: profile.picture,
+					},
+					{
+						element: "span",
+						className: "profileName",
+						dangerouslySetInnerHTML: profile.name,
+					}
+				]
+			}
+		]
 	}	
 ];
